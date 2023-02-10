3028937e6453dc47950270984cd0e46b5f0748af
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2023-02-10 07:15:33 +0300

    Array printing function in lab1

--- lab1/print.c
+++ lab1/print.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void print(int **arr, int n, int *m) {
+void print_arr(int **arr, int n, int *m) {
     // prints an arr with n rows with
     // different sizes
     for (int i = 0; i < n; i++) {
