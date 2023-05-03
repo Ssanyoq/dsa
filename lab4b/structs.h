c48a730580e25ed375030d176613cca905f505d0
Ssanyoq <ssanyoq@yandex.ru>
2023-05-03 17:23:34 +0300

    Node added, unsigned to be decided

--- lab4b/structs.h
+++ lab4b/structs.h
@@ -1,4 +1,14 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+typedef struct Node {
+    int color;
+    struct Node *left;
+    struct Node *right;
+    struct Node *par;
+    unsigned int key;
+    unsigned int val;
+} Node;
+
+
 #endif
